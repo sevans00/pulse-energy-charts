07b85ac2b51af195c7accc7ef12c024207144e08
Shaun Evans <shaunevans.git@gmail.com>
2014-01-20 14:55:17 -0800

    Added colors

--- energychart.js
+++ energychart.js
@@ -1,9 +1,33 @@
 //Strict mode (for now)
 "use strict";
 
+//Imports:
+var colors = require('./node_modules/colors');
+var fs = require('fs');
+
+
+
+
 var apiKey = "9C3B13239D75E73FDE883C934FF647A1";
 var spaceId = process.argv[2];
 
 console.log("Space ID:",spaceId);
 
+
+
+//Read in some data, convert to JSON, print it out, for great justice
+function readSampleData(error, data) {
+	console.log("Error:",error);
+	console.log("Contents of sample:",data);
+	var jsonData = JSON.parse(data);
+	console.log("JSON Data:",jsonData);
+	//Now we need to plot it:
+	
+	//Colors!  Yay!
+	console.log("asdf".green,"fdsa".red);
+}
+
+
+
+fs.readFile("sampleData.txt", readSampleData);
 
